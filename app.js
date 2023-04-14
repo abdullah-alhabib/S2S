320a2787e81696cbb25f38e77a112cca0282b16c
Mutaz1122 <e.mutazalshahrani@gmail.com>
2023-04-14 07:10:46 +0300

    uplod post

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require('express')
 const app = express()
+const multer = require('multer');
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 app.set('view engine', 'ejs');
+
 const port = 3000
 const LocalStrategy = require('passport-local').Strategy;
 var session = require('express-session')
 var passport = require('passport')
 var passportLocalMongoose = require('passport-local-mongoose')
 
+
 const mongoose = require('mongoose');
 
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
+  saveUninitialized: false,
+  // cookie: { secure: false }
 }))
 
 app.use(passport.initialize());
@@ -28,11 +31,14 @@ mongoose.connect('mongodb://127.0.0.1:27017/S2SDb');
 
 //item data base
 
-const itemSchema= new mongoose.Schema({ category: String , name:String , price: Number , building:String ,description:String,img:
-    {
-        data: Buffer,
-        contentType: String
-    }  });
+const itemSchema= new mongoose.Schema({
+  category: String, 
+  name: String, 
+  price: Number, 
+  building: String, 
+  description: String, 
+  
+  });
 const Item = mongoose.model('Item',itemSchema );
 
 // user data base 
@@ -87,23 +93,30 @@ app.get('/register', async (req, res) => {
         
        
       })
-      app.post("/login", function(req, res){
+      app.post('/upload', (req, res) => {
+        console.log(req.body.itemCategory);
+       
 
-        const user = new User({
-          username: req.body.username,
-          password: req.body.password
+
+        const newItem = new Item({
+          category: req.body.itemCategory,
+          name: req.body.itemName,
+          price: req.body.itemPrice,
+          building: req.body.itemBuilding,
+          description: req.body.itemDescription,
         });
-        req.login(user, function(err) {
-            if (err) {   console.log(err);}
-            else{res.redirect('/');
-        }
-             
-          });
-
-      
+        console.log(newItem);
+        newItem.save();
+        res.redirect('/');
+         
       });
+      
 
-    
+      app.post('/login', passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+      }));
+  
 
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
